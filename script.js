0611834fdc64f02ada0d9b7bb1cc30c21fa27f5e
Maycon Padilha <maycon.jordan5000@gmail.com>
2024-10-23 23:57:04 -0300

    adicao do dropdown

--- script.js
+++ script.js
@@ -75,3 +75,31 @@ function gerarListaProjetos(itens) {
     });
 }
 
+function toggleDropdown(element) {
+    const dropdowns = document.querySelectorAll('.menu-item .dropdown-content');
+    dropdowns.forEach(dropdown => {
+        if (dropdown !== element.querySelector('.dropdown-content')) {
+            dropdown.style.display = 'none';
+            dropdown.parentElement.classList.remove('active');
+        }
+    });
+
+    const dropdownContent = element.querySelector('.dropdown-content');
+    const isActive = dropdownContent.style.display === 'block';
+    if (isActive) {
+        dropdownContent.style.display = 'none';
+        element.classList.remove('active');
+    } else {
+        dropdownContent.style.display = 'block';
+        element.classList.add('active');
+    }
+}
+window.onclick = function(event) {
+    if (!event.target.matches('.menu-item, .menu-item *')) {
+        const dropdowns = document.querySelectorAll('.menu-item .dropdown-content');
+        dropdowns.forEach(dropdown => {
+            dropdown.style.display = 'none';
+            dropdown.parentElement.classList.remove('active');
+        });
+    }
+}
